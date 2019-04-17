c493c54c9605efdddca367e046fb2988cbcfde3a
[JZY11] <1291813139@qq.com>
2019-04-17 09:54:02 +0800

    update

---
+++ src/main/java/com/ipubu/cic/chat/segment/IWordSegTerm.java
@@ -0,0 +1,13 @@
+package com.ipubu.cic.chat.segment;
+
+/**
+ * @ClassName IWordSegTerm
+ * @Description
+ * @Author jzy
+ */
+public interface IWordSegTerm extends ITerm{
+	
+	public String getWord();
+	
+	public String getPose();
+}
